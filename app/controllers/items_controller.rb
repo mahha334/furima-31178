c36ca9ac4952bcf0b8ce86d4f15179148c1f8d80
mahha334 <shigeruitou@icloud.com>
2020-11-30 14:48:32 +0900

    コメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,31 +18,31 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-    @item = Item.find(params[:id])
-  end
+  # def show
+  #   @item = Item.find(params[:id])
+  # end
 
-  def edit
-    @item = Items.find(params[:id])
-    unless current_user == @item.user
-      redirect_to root_path
-    end
-  end
+  # def edit
+  #   @item = Items.find(params[:id])
+  #   unless current_user == @item.user
+  #     redirect_to root_path
+  #   end
+  # end
 
-  def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to item_path(@item)
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   @item = Item.find(params[:id])
+  #   if @item.update(item_params)
+  #     redirect_to item_path(@item)
+  #   else
+  #     render :edit
+  #   end
+  # end
 
-  def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
-  end
+  # def destroy
+  #   item = Item.find(params[:id])
+  #   item.destroy
+  #   redirect_to root_path
+  # end
 
   private
 
